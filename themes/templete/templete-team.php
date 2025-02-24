67c4a0654d8e8f57df46cb0a1fb397739069e6ec
城倉禄 <roku@rooph.jp>
2025-02-24 16:22:00 +0900

    add team page

---
+++ themes/templete/templete-team.php
@@ -0,0 +1,179 @@
+<?php
+/*
+Template Name: Team
+*/
+?>
+
+<?php
+$themeUri = get_template_directory_uri();
+?>
+
+<?php get_header(); ?>
+
+<main class="main" role="main">
+<section class="team">
+        <section class="team__mv">
+          <div class="team__mv__inner">
+            <div class="team__mv__ctt">
+              <h1 class="team__mv__title">
+                <span>“新”ポルシェブランド体験を創造する</span>
+                デジタル<br class="sp">クリエイティブチーム
+              </h1>
+              <h2 class="team__mv__heading">
+                EBI DIGITAL STUDIOーそれは、ポルシェに惹かれるすべての人々に向けた、
+                <br class="pc">
+                “ポルシェ・エンターテイメント空間”です。
+              </h2>
+              <div class="team__mv__desc">
+                <p>
+                  私たちはWEBメディアの枠を超え、
+                  <br class="pc">
+                  ポルシェの魅力を心から楽しめる空間を創り上げます。
+                </p>
+                <p>
+                  今、この瞬間も、ポルシェブランドに関する情報は無数にSNSやインターネット上を飛び交っています。
+                  <br class="pc">
+                  しかし、その多くは断片的で、真に“ポルシェに迫る”ような深い洞察を持つ情報は限られています。
+                </p>
+                <p>
+                  EBI GROUPは、日本最大の正規ポルシェディーラーグループとして、
+                  <br class="pc">
+                  25年以上にわたり東京という世界屈指のマーケットでポルシェと向き合い、
+                  <br class="pc">
+                  膨大な知識と経験を積み重ねてきました。
+                </p>
+                <p>
+                  実際にポルシェを所有するオーナーさまや、
+                  <br class="pc">
+                  購入を真剣に考えるお客さまとの唯一無二の接点を持つ私たちだからこそ、
+                  <br class="pc">
+                  本物の“ポルシェブランド体験”を提供できると確信しています。
+                </p>
+                <p>
+                  このデジタルスタジオでは、最新情報の単純な追従ではなく、
+                  <br class="pc">
+                  より本質的なポルシェブランドの真髄を、
+                  <br class="pc">
+                  わかりやすく、そして心躍る形で発信します。
+                </p>  
+                <p>
+                  ただのクルマではない“人生を変える存在”としてのポルシェ。
+                  <br class="pc">
+                  その魅力を余すことなく届け、「ポルシェのある生活」の特別さを、
+                  <br class="pc">
+                  より多くの方に感じてもらえたら——それこそが、私たちの願いです。                              
+                </p>                                              
+              </div>
+              <figure class="team__mv__logo">
+                <img src="/assets/img/team/logo.png" alt="EBI DIGITAL STUDIO">
+              </figure>
+            </div>
+          </div>
+        </section>
+
+        <section class="team__member">
+          <div class="team__member__inner">
+            <div class="team__member__ctt">
+              <h2 class="team__member__heading fontPanchang">
+                MEMBER
+              </h2>
+              <p class="team__member__desc">
+                EBI DIGITAL STUDIOのコンテンツ、制作に関するお問い合わせは [narita [at] lon-lon.co.jp] までお願いいたします。
+                <small>※[at]を@に変更のうえご連絡ください。</small>
+              </p>
+              <ul class="team__member__list">
+                <li class="team__member__item">
+                  <figure class="team__member__itemThumb">
+                    
+                  </figure>
+                  <div class="team__member__itemText">
+                    <p class="team__member__itemPosition fontPanchang">Chief Editor</p>
+                    <p class="team__member__itemNameEn fontPanchang">Tatsuhiko Kanno</p>
+                    <h3 class="team__member__itemNameJa">菅野 竜彦</h3>                    
+                    <p class="team__member__itemScope fontPanchang">
+                      <span>Scope</span>
+                      Produce / Direction / Edit / Writing / Photographer
+                    </p>
+                    <p class="team__member__itemCompany fontPanchang">
+                      EBI Marketing Inc. / Sanji international Co.,Ltd.
+                    </p>
+                  </div>
+                </li>
+                <li class="team__member__item">
+                  <figure class="team__member__itemThumb">
+                    
+                  </figure>
+                  <div class="team__member__itemText">
+                    <p class="team__member__itemPosition fontPanchang">Chief Coorinator / PORSCHE PRO</p>
+                    <p class="team__member__itemNameEn fontPanchang">Youichi Johannes Sasaki</p>
+                    <h3 class="team__member__itemNameJa">佐々木ヨハネス陽一</h3>                    
+                    <p class="team__member__itemScope fontPanchang">
+                      <span>Scope</span>
+                      Coordination
+                    </p>
+                    <p class="team__member__itemCompany fontPanchang">
+                      EBI Marketing Inc.
+                    </p>
+                  </div>
+                </li>                
+                <li class="team__member__item">
+                  <figure class="team__member__itemThumb">
+                    
+                  </figure>
+                  <div class="team__member__itemText">
+                    <p class="team__member__itemPosition fontPanchang">Director</p>
+                    <p class="team__member__itemNameEn fontPanchang">Ryuya Narita</p>
+                    <h3 class="team__member__itemNameJa">成田 龍矢</h3>                    
+                    <p class="team__member__itemScope fontPanchang">
+                      <span>Scope</span>
+                      Produce / Direction
+                    </p>
+                    <p class="team__member__itemCompany fontPanchang">
+                      LON Inc. / CREATOR BOX Inc.
+                    </p>
+                  </div>
+                </li>                
+                <li class="team__member__item">
+                  <figure class="team__member__itemThumb">
+                    
+                  </figure>
+                  <div class="team__member__itemText">
+                    <p class="team__member__itemPosition fontPanchang">Editor / Writer</p>
+                    <p class="team__member__itemNameEn fontPanchang">Ryuya Narita</p>
+                    <h3 class="team__member__itemNameJa">成田 龍矢</h3>                    
+                    <p class="team__member__itemScope fontPanchang">
+                      <span>Scope</span>
+                      Edit / Writting
+                    </p>
+                    <p class="team__member__itemCompany fontPanchang">
+                      CREATOR BOX Inc.
+                    </p>
+                  </div>
+                </li>                
+                <li class="team__member__item">
+                  <figure class="team__member__itemThumb">
+                    
+                  </figure>
+                  <div class="team__member__itemText">
+                    <p class="team__member__itemPosition fontPanchang">Photographer</p>
+                    <p class="team__member__itemNameEn fontPanchang">Shizuka Sherry</p>
+                    <h3 class="team__member__itemNameJa">シズカシェリー</h3>                    
+                    <p class="team__member__itemScope fontPanchang">
+                      <span>Scope</span>
+                      Photographer 
+                    </p>
+                    <p class="team__member__itemCompany fontPanchang">
+                      CREATOR BOX Inc.
+                    </p>
+                  </div>
+                </li>                
+
+              </ul>
+
+            </div>
+          </div>
+        </section>
+  </section>
+</main>
+
+<?php get_footer(); ?>
