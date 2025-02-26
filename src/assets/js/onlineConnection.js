5f76b71a66ef0d993bec611d903e7168387ce4ac
eisuke namba <namba@yaya.tokyo>
2025-02-26 20:15:27 +0900

    Online Connection バリデーション

--- src/assets/js/onlineConnection.js
+++ src/assets/js/onlineConnection.js
@@ -11,23 +11,23 @@ export default class OnlineConnection {
       const inputed = prompt(
         'ONLINE CONNECTIONについて\n※こちらはPORSCHE PRO案内のもと、ご利用ください。\nパスワードをご入力の上、ご入室ください。'
       );
-      if (!!inputed) {
-        open(
-          `https://zoom.us/j/92840087470?pwd=NVNZU1JGS0ZGclpNUHhqUzduQVlIQ${inputed}`,
-          '_blank'
-        );
-      }
-
-      // if (/\W+/g.test(inputed)) {
-      //   alert('半角英数字のみを入力して下さい。');
-      // } else if (inputed == '') {
-      //   alert('正しいパスワードを入力ください。');
-      // } else if (inputed != null) {
+      // if (!!inputed) {
       //   open(
       //     `https://zoom.us/j/92840087470?pwd=NVNZU1JGS0ZGclpNUHhqUzduQVlIQ${inputed}`,
       //     '_blank'
       //   );
       // }
+
+      if (/\W+/g.test(inputed)) {
+        alert('半角英数字のみを入力して下さい。');
+      } else if (inputed == '') {
+        alert('正しいパスワードを入力ください。');
+      } else if (inputed !== null) {
+        open(
+          `https://zoom.us/j/92840087470?pwd=NVNZU1JGS0ZGclpNUHhqUzduQVlIQ${inputed}`,
+          '_blank'
+        );
+      }
     });
   }
 }
